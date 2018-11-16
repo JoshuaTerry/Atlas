121ff26af5b219ed3203cc19ca4830b65847e7ca
mreynolds <mreynolds@drivecentric.com>
2018-11-16 11:11:34 -0500

    updated interfaces to models

---
+++ Model/DriveServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+using ServiceStack.DataAnnotations;
+
+namespace DriveCentric.Model
+{
+    public class DriveServer : IBaseModel
+    {
+        [PrimaryKey]
+        [JsonIgnore]
+        [Alias("pkDriveServerID")]
+        public int Id { get; set; }
+
+        [Alias("GUID")]
+        public Guid ExternalId { get; set; }
+
+        [Alias("ConnectionString")]
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Model/Store.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using ServiceStack.DataAnnotations;
+
+namespace DriveCentric.Model
+{
+    public class Store : IBaseModel
+    {
+        [PrimaryKey]
+        [JsonIgnore]
+        [Alias("pkStoreID")]
+        public int Id { get; set; }
+
+        [Alias("GUID")]
+        public Guid ExternalId { get; set; }
+    }
+}
